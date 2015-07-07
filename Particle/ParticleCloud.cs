7daf538da54abd4867ffe9dce3bc3487212b65f6
Adam Holt <holtsoftware@outlook.com>
2015-07-07 09:33:05 -0600

    Started adding classes.

---
+++ Particle/ParticleCloud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Particle
+{
+	public class ParticleCloud
+	{
+		public static ParticleCloud SharedInstance
+		{
+			get
+			{
+				return null;
+			}
+		}
+	}
+}
