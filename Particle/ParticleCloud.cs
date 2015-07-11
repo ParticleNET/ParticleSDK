3dfa807f73abdaa1ea7db2353acff8542e4c055e
Adam Holt <holtsoftware@outlook.com>
2015-07-10 23:14:36 -0600

    Added License to top of files and added planed particlecloud methods.

--- Particle/ParticleCloud.cs
+++ Particle/ParticleCloud.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using Particle.Results;
-/*
+﻿/*
 Copyright 2015 Sannel Software, L.L.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
+using Newtonsoft.Json;
+using Particle.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +30,8 @@ namespace Particle
 		private Uri baseUri;
 		private AuthenticationResults authResults;
 
-		public ParticleCloud() : this(new Uri("https://api.particle.io/"))
+		public ParticleCloud()
+			: this(new Uri("https://api.particle.io/"))
 		{
 
 		}
@@ -52,9 +53,10 @@ namespace Particle
 			{
 				throw new ArgumentNullException("password");
 			}
-			
+
 			client.DefaultRequestHeaders.Clear();
 			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes("particle:particle")));
+
 			var data = new Dictionary<String, String>();
 			data["grant_type"] = "password";
 			data["username"] = username;
@@ -96,10 +98,80 @@ namespace Particle
 				Error = postResults.StatusCode.ToString()
 			};
 		}
+		/// <summary>
+		/// Sign up with new account credentials to Spark cloud
+		/// </summary>
+		/// <param name="username">Required user name, must be a valid email address</param>
+		/// <param name="password">Required password</param>
+		/// <returns></returns>
+		//public async Task<SignupResults> SignupWithUserAsync(String username, String password)
+		//{
+
+		//}
+
+		/// <summary>
+		/// Logs the user out locally
+		/// </summary>
+		public void Logout()
+		{
+			authResults = null;
+		}
+
+		/// <summary>
+		/// Get the list of devices the user has claimed
+		/// </summary>
+		/// <returns></returns>
+		//public async Task<List<ParticleDevice>> GetDevicesAsync()
+		//{
+
+		//}
+
+		/// <summary>
+		/// Gets the device with the id equal to <paramref name="deviceId"/>
+		/// or returns null if its not found.
+		/// </summary>
+		/// <param name="deviceId">The id of the device to get</param>
+		/// <returns>The device</returns>
+		//public async Task<ParticleDevice> GetDeviceAsync(String deviceId)
+		//{
+
+		//}
+
+		/// <summary>
+		/// Not available Yet
+		/// </summary>
+		/// <param name="eventName"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		//public async Task PublishEvent(String eventName, System.IO.Stream data)
+		//{
+
+		//}
+
+		/// <summary>
+		/// Claims the specified device for the logged in user
+		/// </summary>
+		/// <param name="deviceId">The id of the new device</param>
+		/// <returns></returns>
+		//public async Task<ClaimResult> ClaimDeviceAsync(String deviceId)
+		//{
+
+		//}
+
+		/// <summary>
+		/// Get a short-lived claiming token for transmitting to soon-to-be-claimed device in soft AP setup process
+		/// </summary>
+		/// <returns></returns>
+		//public async Task<ClaimCodeResult> GenerateClaimCodeAsync()
+		//{
+
+		//}
+
+
 
 		public void Dispose()
 		{
-			
+			client.Dispose();
 		}
 	}
 }
--- ParticleSDKTests/ParticleCloudTests.cs
+++ ParticleSDKTests/ParticleCloudTests.cs
@@ -13,18 +13,20 @@ namespace ParticleSDKTests
 		[TestMethod]
 		public async Task AuthenticationTestAsync()
 		{
-			var cloud = new ParticleCloud();
-			var exc = AssertHelpers.ThrowsException<ArgumentNullException>(() => { cloud.LoginWithUserAsync(null, "sadf").GetAwaiter().GetResult(); });
-			Assert.AreEqual("username", exc.ParamName);
-			exc = AssertHelpers.ThrowsException<ArgumentNullException>(() => { cloud.LoginWithUserAsync("test", null).GetAwaiter().GetResult(); });
-			Assert.AreEqual("password", exc.ParamName);
+			using (var cloud = new ParticleCloud())
+			{
+				var exc = AssertHelpers.ThrowsException<ArgumentNullException>(() => { cloud.LoginWithUserAsync(null, "sadf").GetAwaiter().GetResult(); });
+				Assert.AreEqual("username", exc.ParamName);
+				exc = AssertHelpers.ThrowsException<ArgumentNullException>(() => { cloud.LoginWithUserAsync("test", null).GetAwaiter().GetResult(); });
+				Assert.AreEqual("password", exc.ParamName);
 
-			var results = await cloud.LoginWithUserAsync("test@test.com", "test123");
-			Assert.IsFalse(results.IsAuthenticated, "User some how authenticated?");
-			Assert.AreEqual("User credentials are invalid", results.Error);
+				var results = await cloud.LoginWithUserAsync("test@test.com", "test123");
+				Assert.IsFalse(results.IsAuthenticated, "User some how authenticated?");
+				Assert.AreEqual("User credentials are invalid", results.Error);
 
-			results = await cloud.LoginWithUserAsync(ConfigurationManager.AppSettings["Username"], ConfigurationManager.AppSettings["Password"]);
-			Assert.IsTrue(results.IsAuthenticated, "User did not authenticate");
+				results = await cloud.LoginWithUserAsync(ConfigurationManager.AppSettings["Username"], ConfigurationManager.AppSettings["Password"]);
+				Assert.IsTrue(results.IsAuthenticated, "User did not authenticate");
+			}
 		}
 	}
 }
