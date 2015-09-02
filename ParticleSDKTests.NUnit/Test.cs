c02895f2b393b4e7ffd49bae08039b5a39d053c5
Adam Holt <holtsoftware@outlook.com>
2015-09-02 14:16:39 -0600

    Added NUnit project so we can run unit test on windows and linux.

---
+++ ParticleSDKTests.NUnit/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ParticleSDKTests.NUnit
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			
+		}
+	}
+}
+
